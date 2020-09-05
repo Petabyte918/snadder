5b16d8e3dd342952ac600a5a0264f2551c91fd4f
sidd5sci <sidd5sci@gmail.com>
2020-09-05 23:36:23 +0530

    progress bar added

--- js/scenes/splash.js
+++ js/scenes/splash.js
@@ -14,6 +14,45 @@ var Splash = new Phaser.Class({
 
     preload: function ()
     {
+
+        var progressBar = this.add.graphics();
+        var progressBox = this.add.graphics();
+        progressBox.fillStyle(0x222222, 0.8);
+        progressBox.fillRect(340, 840, 320, 50);
+        var width = this.cameras.main.width;
+        var height = this.cameras.main.height;
+        var loadingText = this.make.text({
+            x: width / 2,
+            y: height / 2 - 50,
+            text: 'Loading...',
+            style: {
+                font: '20px monospace',
+                fill: '#ffffff'
+            }
+        });
+        loadingText.setOrigin(0.5, 0.5);
+        var percentText = this.make.text({
+            x: width / 2,
+            y: height / 2 - 5,
+            text: '0%',
+            style: {
+                font: '18px monospace',
+                fill: '#ffffff'
+            }
+        });
+        percentText.setOrigin(0.5, 0.5);
+        var assetText = this.make.text({
+            x: width / 2,
+            y: height / 2 + 50,
+            text: '',
+            style: {
+                font: '18px monospace',
+                fill: '#ffffff'
+            }
+        });
+        assetText.setOrigin(0.5, 0.5);
+
+
         this.load.image('sky', 'assets/images/sky.png');
         this.load.image('bg0','assets/images/BG_Decor.png');
         this.load.image('bg1','assets/images/Middle_Decor.png');
@@ -23,10 +62,54 @@ var Splash = new Phaser.Class({
         this.load.image('tile', 'assets/images/tile.png');
         this.load.image('snakes', 'assets/images/snakes0.png');
 
-        this.load.image('play','assets/images/UI/btn/play.png');
+        this.load.image('btn_play','assets/images/UI/btn/play.png');
+        this.load.image('btn_about','assets/images/UI/btn/about.png');
+        this.load.image('btn_close','assets/images/UI/btn/close.png');
+        this.load.image('btn_close1','assets/images/UI/btn/close_2.png');
+        this.load.image('btn_faq','assets/images/UI/btn/faq.png');
+        this.load.image('btn_leader','assets/images/UI/btn/leader.png');
+        this.load.image('btn_menu','assets/images/UI/btn/menu.png');
+        this.load.image('btn_misic','assets/images/UI/btn/misic.png');
+        this.load.image('btn_misic_off','assets/images/UI/btn/misic_off.png');
+        this.load.image('btn_next','assets/images/UI/btn/next.png');
+        this.load.image('btn_ok','assets/images/UI/btn/ok.png');
+        this.load.image('btn_pause','assets/images/UI/btn/pause.png');
+        this.load.image('btn_play','assets/images/UI/btn/play.png');
+        this.load.image('btn_prew','assets/images/UI/btn/prew.png');
+        this.load.image('btn_prize','assets/images/UI/btn/prize.png');
+        this.load.image('btn_restart','assets/images/UI/btn/restart.png');
+        this.load.image('btn_settings','assets/images/UI/btn/settings.png');
+        this.load.image('btn_shop','assets/images/UI/btn/shop.png');
+        this.load.image('btn_sound','assets/images/UI/btn/sound.png');
+        this.load.image('btn_sound_off','assets/images/UI/btn/sound_off.png');
+        this.load.image('btn_upgrade','assets/images/UI/btn/upgrade.png');
 
         this.load.bitmapFont('fire','assets/fonts/bitmap/azo-fire.png','assets/fonts/bitmap/azo-fire.xml');
         this.load.bitmapFont('green','assets/fonts/bitmap/shortStack.png','assets/fonts/bitmap/shortStack.xml');
+    
+        this.load.on('progress', function (value) {
+            // console.log(value);
+            progressBar.clear();
+            progressBar.fillStyle(0xffffff, 1);
+            progressBar.fillRect(350, 850, 300 * value, 30);
+            percentText.setText(parseInt(value * 100) + '%');
+        });
+                    
+        this.load.on('fileprogress', function (file) {
+            // console.log(file.src);
+            assetText.setText('Loading asset: ' + file.key);
+            // assetText.setText('Loading asset: ' + file.src);
+        });
+         
+        this.load.on('complete', function () {
+            // console.log('complete');
+            progressBar.destroy();
+            progressBox.destroy();
+            loadingText.destroy();
+            percentText.destroy();
+            assetText.destroy();
+        });
+    
     },
 
     create: function ()
@@ -45,9 +128,9 @@ var Splash = new Phaser.Class({
 
         this.snake = this.add.image(480, 900, 'snakes').setScale(0.7);
         // this.add.text(330, 1100,'START', { font: '100px Arial', fill: '#fff'});
-        this.add.image(480,1200,'play');
+        this.add.image(480,1200,'btn_play');
         this.snadder = this.add.dynamicBitmapText(150,500,'fire','SNADDER',128);
-        this.snadder.setDisplayCallback(this.waveAnimation,this.phase);
+        // this.snadder.setDisplayCallback(this.waveAnimation,this.phase);
         
         this.input.once('pointerdown', function () {
         
@@ -55,21 +138,20 @@ var Splash = new Phaser.Class({
 
         }, this);
 
-        console.log(this.num)
 
         // window.splashScene = this;
         // window.splashScene.get('Splash').myMethod();
     },
-    waveAnimation:function(data,phase){
-        console.log("d",data);
-        data.y = parseInt(Math.cos(phase+0.5) *10);
-        console.log(Math.cos(0),phase);
-        phase += 0.01;
-        // return data;
-    },
-    update:function(){
+    // waveAnimation:function(data,phase){
+    //     console.log("d",data);
+    //     data.y = parseInt(Math.cos(phase+0.5) *10);
+    //     console.log(Math.cos(0),phase);
+    //     phase += 0.01;
+    //     // return data;
+    // },
+    // update:function(){
         
-    }
+    // }
 
 
 });
